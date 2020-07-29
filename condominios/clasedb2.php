9ad7555d31ac7aa0afb95dcb10f0b118dd077146
lieskadejesus <ladjd2012@gmail.com>
2020-07-29 15:38:48 -0400

    mi primer commit

--- condominios/clasedb2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class clasedb2{
-		private $db;
-		public function conectar(){
-			$this->db=new mysqli("localhost","root","","condominio") or die ("No se pudo conectar con Mysql");
-			return $this->db;
-		}
-	}
-?>
